632e6a9905d2c8a1f6bac12bb06a88b1b77703a1
GerardoAGL96 <gerardoagl96@gmail.com>
2019-12-16 22:06:36 -0400

    Content moderation on update

--- resources/js/components/organisms/Wall.js
+++ resources/js/components/organisms/Wall.js
@@ -18,8 +18,10 @@ class Wall extends React.Component  {
     }
 
     this.onFrameLoad = this.onFrameLoad.bind(this);
+    this.onPostsUpdate = this.onPostsUpdate.bind(this);
 
     this.intervalId = '';
+    this.observer = new MutationObserver(this.onPostsUpdate);
     this.iFrameRef = React.createRef();
     this.socialURL = window.location.protocol + "//" + window.location.host + "/Lib";
   }
@@ -46,12 +48,28 @@ class Wall extends React.Component  {
     }
   }
 
+  componentWillUnmount () {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  /**
+   * Una vez traidas las publicaciones, llama al moderador y revisa cada una de las publicaciones
+   * que inicialmente llegaron al wall
+   */
   onFrameLoad (e) {
     /**
-     * Filter only by twitter, instagram and rss posts
+     * Filtra para que solo aparezcan las publicaciones que tienen la clase
+     * correspondiente a twitter, instagram o rss
      */
     this.iFrameRef.current.contentDocument.getElementsByClassName("filter-label")[0].setAttribute('data-filter', '.sb-twitter, .sb-instagram, .sb-rss');    
     const rawPosts = this.iFrameRef.current.contentDocument.getElementsByClassName("sb-item");
+    
+    this.observer.observe(this.iFrameRef.current.contentDocument.getElementById('timeline_wall1'), { 
+      childList: true, 
+    });
+
     let posts = [];
     
     for (let post of rawPosts) {
@@ -105,6 +123,8 @@ class Wall extends React.Component  {
 
           Promise.all(promises).then(([imgModeration, txtModeration]) => {
             if (imgModeration.data.IsImageAdultClassified || imgModeration.data.IsImageRacyClassified || txtModeration.data.Terms) {
+              // Le quita la clase de la red social de la cual
+              // vino la publicación para que no aparezca más en pantalla
               this.iFrameRef.current.contentDocument.getElementById(post.id).classList.remove(post.type);
             }
           });
@@ -113,6 +133,61 @@ class Wall extends React.Component  {
         }
 
       }, 1000);
+    });
+  }
+
+  onPostsUpdate (mutationList, observer) {
+    const [record] = mutationList;
+    let posts = [];
+
+    record.addedNodes.forEach(node => posts.push({
+      id: node.id,
+      type: node.classList.item(1),
+      image: node.getElementsByClassName('icbox').length > 0 ? node.getElementsByClassName('icbox')[0].href : null,
+      text: node.getElementsByClassName("sb-text")[0] ? node.getElementsByClassName("sb-text")[0].innerText : "",
+    }));
+
+    posts = posts.filter((post, i) => {
+      const exists = this.state.posts.find(p => p.id === post.id) ? true : false;
+
+      if (exists) {
+        return false;        
+      } else {
+        record.addedNodes[i].classList.remove(post.id);
+        return true;
+      }
+    });
+
+    this.iFrameRef.current.contentDocument.getElementsByClassName("filter-label")[0].click();
+
+    const promises = posts.map(post => new Promise((resolve, reject) => {
+      if (post.text) {
+        this.props.doesTextNeedModeration(post.text).then(res => {
+          if (!res.data.Terms) {
+            this.iFrameRef.current.contentDocument.getElementById(post.id).classList.add(post.type);
+          }
+
+          resolve({});
+        });
+      } else {
+        resolve({});
+      }
+    }));
+
+    Promise.all(promises).then(() => {
+      this.iFrameRef.current.contentDocument.getElementsByClassName("filter-label")[0].click();
+
+      console.log('Done');
+
+      this.setState(state => ({
+        posts: [
+          ...state.posts,
+          ...posts
+        ]
+      }))
+    })
+    .catch(err => {
+      this.iFrameRef.current.contentDocument.getElementsByClassName("filter-label")[0].click();
     });
   }
 
