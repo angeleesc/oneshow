e9c788f92bc39811b671841218d22a197d0a8ebc
Jesus Sanchez <js868937@gmail.com>
2020-01-09 09:11:59 -0400

    Inclusión de opción MosaicWall en menú

--- app/Http/Controllers/BibliotecaController.php
+++ app/Http/Controllers/BibliotecaController.php
@@ -644,6 +644,23 @@ class BibliotecaController extends Controller
                 }
             }
 
+            // Envíar petición curl
+            $data = ['empresa' => $empresa, 'evento' => $evento];
+
+            $data_string = json_encode($data);
+
+            $ch = curl_init(env('ONESHOW_MOSAIC_SERVER'));
+                                                                 
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                'Content-Type: application/json',                                                                                
+                'Content-Length: ' . strlen($data_string))                                                                 
+            );
+            curl_exec($ch);
+            curl_close($ch);
+
             return response()->json([
                 'code' => 200
             ]);
